beb7b6a4890e6d2e5f91b74f081f87776bc6841b
David Cramer <dcramer@gmail.com>
2008-05-12 20:34:00 +0000

    added distutils files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='djangoratings',
+      version='1.0',
+      description='Generic Ratings in Django',
+      author='David Cramer',
+      author_email='dcramer@gmail.com',
+      url='http://code.curse.com/p/django-ratings/',
+      packages=['djangoratings'],
+     )
