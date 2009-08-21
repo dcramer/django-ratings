5a5389d49b7d8aceea1bc52b04a8fccb0313d526
David Cramer <dcramer@david-cramers-macbook.local>
2009-08-21 16:45:42 -0500

    Updated admin to include date_changed

--- djangoratings/admin.py
+++ djangoratings/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from models import Vote, Score
 
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('content_object', 'user', 'ip_address', 'score')
-    list_filter = ('score', 'content_type')
+    list_display = ('content_object', 'user', 'ip_address', 'score', 'date_changed')
+    list_filter = ('score', 'content_type', 'date_changed')
     search_fields = ('ip_address',)
 
 class ScoreAdmin(admin.ModelAdmin):
