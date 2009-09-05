ac833fa4dc5ab99f2ef14277be87467e3cb3de83
David Cramer <dcramer@david-cramers-macbook.local>
2009-09-04 19:56:12 -0500

    added djangoratings.__build__

--- djangoratings/__init__.py
+++ djangoratings/__init__.py
@@ -1,4 +1,30 @@
+import os.path
+
 __version__ = (0, 3, 0)
+
+def _get_git_revision(path):
+    revision_file = os.path.join(path, 'refs', 'heads', 'master')
+    if not os.path.exists(revision_file):
+        return None
+    fh = open(revision_file, 'r')
+    try:
+        return fh.read()
+    finally:
+        fh.close()
+
+def get_revision():
+    """
+    :returns: Revision number of this branch/checkout, if available. None if
+        no revision number can be determined.
+    """
+    package_dir = os.path.dirname(__file__)
+    checkout_dir = os.path.normpath(os.path.join(package_dir, '..'))
+    path = os.path.join(checkout_dir, '.git')
+    if os.path.exists(path):
+        return _get_git_revision(path)
+    return None
+
+__build__ = get_revision()
 
 from django.db.models import IntegerField, PositiveIntegerField
 from django.conf import settings
