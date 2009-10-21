19b24c0520cd3daa2da979da3d444f21bad12d7e
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-10-21 15:26:44 -0500

    increment version to 0.3.4 for views fix

--- djangoratings/__init__.py
+++ djangoratings/__init__.py
@@ -1,7 +1,7 @@
 import os.path
 import warnings
 
-__version__ = (0, 3, 3)
+__version__ = (0, 3, 4)
 
 def _get_git_revision(path):
     revision_file = os.path.join(path, 'refs', 'heads', 'master')
