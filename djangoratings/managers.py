0c4e94cf9f6265768178509a4dcd07d1f502f5c8
David Cramer <dcramer@David-Cramers-MacBook.local>
2009-12-27 18:47:33 -0600

    Fix for the return value of delete method

--- djangoratings/managers.py
+++ djangoratings/managers.py
@@ -17,13 +17,15 @@ class VoteQuerySet(QuerySet):
             ct = ContentType.objects.get(pk=content_type)
             to_update.extend(list(ct.model_class().objects.filter(pk__in=list(objects)[0])))
         
-        super(VoteQuerySet, self).delete(*args, **kwargs)
+        retval = super(VoteQuerySet, self).delete(*args, **kwargs)
         
         # TODO: this could be improved
         for obj in to_update:
             for field in getattr(obj, '_djangoratings', []):
                 getattr(obj, field.name)._update()
             obj.save()
+        
+        return retval
         
 class VoteManager(Manager):
     def get_query_set(self):
