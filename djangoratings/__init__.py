70830ec06daf92713c5ce6b8467c0e80d42cd51b
David Cramer <dcramer@gmail.com>
2011-03-16 15:50:47 -0700

    Bump version to 0.3.6

--- djangoratings/__init__.py
+++ djangoratings/__init__.py
@@ -1,7 +1,7 @@
 import os.path
 import warnings
 
-__version__ = (0, 3, 5)
+__version__ = (0, 3, 6)
 
 def _get_git_revision(path):
     revision_file = os.path.join(path, 'refs', 'heads', 'master')
