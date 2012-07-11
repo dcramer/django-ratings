7c31b30b539f328140db70e5f4108481cccb401f
David Cramer <dcramer@gmail.com>
2012-07-11 15:36:19 +0200

    0.3.7

--- djangoratings/__init__.py
+++ djangoratings/__init__.py
@@ -1,7 +1,7 @@
 import os.path
 import warnings
 
-__version__ = (0, 3, 6)
+__version__ = (0, 3, 7)
 
 def _get_git_revision(path):
     revision_file = os.path.join(path, 'refs', 'heads', 'master')
